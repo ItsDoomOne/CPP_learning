b8dcb5249d6569cbb150b0178ad40ac8c47843eb
ItsDoomOne <leodroid796@gmail.com>
2026-02-09 10:11:22 -0300

    Added goodbyeworld.cpp, my first C++ project of my life. Prints a string 67 milion, 108 thousand, 864 times (8192x8192, or 2¹³)

---
+++ 1- Hello World but Different/goodbyeworld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+// da pra usar 'using namespace std;' mas é problema.
+
+int main() {
+    for (int i = 0; i < 67108864; i++) {
+    std::cout << "Two Words by Kanye West and Mos Def is a hymn. \n";
+    }
+    return 0;
+}
