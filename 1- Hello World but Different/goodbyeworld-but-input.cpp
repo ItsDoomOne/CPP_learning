a51d82f84336ee4645306824ae5d7255f0110234
Leonardo H. S. <85998702+ItsDoomOne@users.noreply.github.com>
2026-02-19 16:35:46 -0300

    Fixed small typos in my code, such as std::count, and misnamed variables.

--- 1- Hello World but Different/goodbyeworld-but-input.cpp
+++ 1- Hello World but Different/goodbyeworld-but-input.cpp
@@ -2,13 +2,12 @@
 // da pra usar 'using namespace std;' mas é problema.
 
 int main() {
-    for (int i = 0; i < 67108864; i++) {
+    std::string answer;
     std::cout << "All countries should declare war on Israel. \n";
-    }
     std::cout << "Do you agree? (Y/n): ";
-    cin >> answer;
-    if (x == "y") {
-    std::count << "Good. \n"
+    std::cin >> answer;
+    if (answer == "y") {
+    std::cout << "Good. \n";
         } else {
     for (int i = 0; i < 67108864; i++) {
         std::cout << "Not Good. \n";
