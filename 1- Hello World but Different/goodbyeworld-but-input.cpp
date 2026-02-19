e084d6b5df94d5fadc14f005690b5703de599b63
Leonardo H. S. <85998702+ItsDoomOne@users.noreply.github.com>
2026-02-19 16:32:07 -0300

    New script that tests input. Never did this before.
    Small changed over goodbyeworld.cpp that add additional functionality

---
+++ 1- Hello World but Different/goodbyeworld-but-input.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+// da pra usar 'using namespace std;' mas é problema.
+
+int main() {
+    for (int i = 0; i < 67108864; i++) {
+    std::cout << "All countries should declare war on Israel. \n";
+    }
+    std::cout << "Do you agree? (Y/n): ";
+    cin >> answer;
+    if (x == "y") {
+    std::count << "Good. \n"
+        } else {
+    for (int i = 0; i < 67108864; i++) {
+        std::cout << "Not Good. \n";
+    }
+    }
+    return 0;
+}
